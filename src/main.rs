c09cd1d97a2d4c4cd12111688c072e8546de3706
Hendrik Müller <henne90gen@gmail.com>
2020-06-19 16:09:53 +0200

    Tried to implement checker board sorting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate image;
 extern crate itertools;
 extern crate num_complex;
+use crate::image::GenericImage;
+use crate::image::GenericImageView;
 use itertools::Itertools;
 
 type Criteria = fn(&image::Rgb<u8>) -> u8;
@@ -13,6 +15,9 @@ fn get_green(item: &image::Rgb<u8>) -> u8 {
 }
 fn get_blue(item: &image::Rgb<u8>) -> u8 {
     item.0[2]
+}
+fn get_average(item: &image::Rgb<u8>) -> u8 {
+    (item.0[0] + item.0[1] + item.0[2]) / 3
 }
 
 type Sorter = fn(
@@ -34,6 +39,46 @@ fn basic_sort(
     let width = buf.width();
     let height = buf.height();
     image::ImageBuffer::from_vec(width, height, sorted_pixels).unwrap()
+}
+
+fn checker_sort(
+    buf: &image::ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>>,
+    crit: Criteria,
+) -> image::ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>> {
+    let image_width = buf.width();
+    let image_height = buf.height();
+
+    let mut result: image::ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>> =
+        image::ImageBuffer::new(image_width, image_height);
+
+    let width = image_width / 10;
+    let height = image_height / 10;
+    for y in 0..10 {
+        for x in 0..10 {
+            let x_offset = x * width;
+            let y_offset = y * height;
+            let sorted_pixels: Vec<(u32, u32, image::Rgb<u8>)> = buf
+                .view(x_offset, y_offset, width, height)
+                .pixels()
+                .sorted_by_key(|p| crit(&p.2))
+                /*.flat_map(|p| {
+                    let pixel = p.2;
+                    let mut data = Vec::new();
+                    for a in pixel.0.iter() {
+                        data.push(*a);
+                    }
+                    data
+                })*/
+                .clone()
+                .collect();
+            let mut sub_image = result.sub_image(x_offset, y_offset, width, height);
+            for (x_, y_, p) in sorted_pixels {
+                sub_image.put_pixel(x_, y_, p);
+            }
+        }
+    }
+
+    result
 }
 
 fn sort_image(
@@ -60,7 +105,7 @@ fn sort_image(
         None => {
             println!();
             return;
-        },
+        }
         Some(x) => x.join(dir_name),
     };
     if !dir_path.is_dir() {
@@ -92,12 +137,14 @@ fn sort_image(
 fn main() {
     let mut sorters: std::collections::HashMap<String, Sorter> = std::collections::HashMap::new();
     sorters.insert("Basic".to_string(), basic_sort);
+    sorters.insert("Checker".to_string(), checker_sort);
 
     let mut criterias: std::collections::HashMap<String, Criteria> =
         std::collections::HashMap::new();
     criterias.insert("Red".to_string(), get_red);
     criterias.insert("Green".to_string(), get_green);
     criterias.insert("Blue".to_string(), get_blue);
+    criterias.insert("Average".to_string(), get_average);
 
     let root_dir = std::path::Path::new("res");
     let dirs = match std::fs::read_dir(root_dir) {
@@ -107,6 +154,8 @@ fn main() {
         }
         Ok(x) => x,
     };
+
+    let mut handles = Vec::new();
     for entry in dirs {
         let dir = match entry {
             Err(e) => {
@@ -119,6 +168,15 @@ fn main() {
         if image_name.is_dir() {
             continue;
         }
-        sort_image(&sorters, &criterias, &image_name)
+        let s = sorters.clone();
+        let c = criterias.clone();
+        let handle = std::thread::spawn(move || {
+            sort_image(&s, &c, &image_name);
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 }
